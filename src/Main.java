5a3c247b589c889252a87df6f9519cdc0f7752c1
RenilsonLuna <renilsonjunior1@gmail.com>
2023-11-10 10:48:50 -0300

    Conexao testada

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
-import java.util.HashSet;
-import java.util.Set;
+// import java.util.HashSet;
+// import java.util.Set;
+import conexao.Database;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        Set<String> Categorias = new HashSet<>();
+        // System.out.println("Hello world!");
+        // Set<String> Categorias = new HashSet<>();
+        new Database();
     }
 }
