6793f6a1d653a3913c9d7aaaa57a4ce55710aef4
RenilsonLuna <renilsonjunior1@gmail.com>
2023-11-21 08:05:12 -0300

    Padrao arquitetural MVC implementado

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 import view.interfaceGrafica.Tela;
 
+
 public class Main {
     public static void main(String[] args) {
         Tela tela = Tela.getInstance(800, 1000);
         
-        //ConexaoImpl conn = new ConexaoImpl();
+        // ConexaoImpl conn = new ConexaoImpl();
         // UserConnectionImpl.setConexao(conn.getConexao());
 
     }
