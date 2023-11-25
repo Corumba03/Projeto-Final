bf8a5b9871c5ddfd24d652d42f770de7e5840490
RenilsonLuna <renilsonjunior1@gmail.com>
2023-11-25 19:19:59 -0300

    tipoPacote retirado

--- src/controller/usuarios/AdminController.java
+++ src/controller/usuarios/AdminController.java
@@ -2,15 +2,8 @@ package controller.usuarios;
 
 public class AdminController extends UsuarioController {
 
-    public AdminController(String cpf, String nome, char[] senha, String email, int tipoPacote) {
+    public AdminController(String cpf, String nome, char[] senha, String email) {
         super(cpf, nome, senha, email);
     }
 
-    public void adicionarUsuario(UsuarioController usuario){
-        // TODO criar usuario a partir do objeto usuario
-    }
-    
-    public void editarUsuario(UsuarioController usuario) {
-        // TODO fazer switch case para decidir qual dado de qual usuario alterar
-    }
 }
